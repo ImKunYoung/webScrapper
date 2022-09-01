6b72cf73cb83cdd9599e6e13c65658acdc5fe8bc
ImKunYoung <lim8667@gmail.com>
2022-09-01 22:56:04 +0900

    Else & Elif

--- main.py
+++ main.py
@@ -212,3 +212,18 @@ a = 10
 if a>5:
     print("collect")
 
+# Else & Elif
+password_correct = False
+
+if password_correct:
+    print("Here is your money")
+else:
+    print("Wrong password")
+
+winner = 4
+if winner > 10:
+    print("Winner is greater than 10")
+elif winner < 10:
+    print("Winner is less than 10")
+else:
+    print("Winner is 10")
